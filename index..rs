02a836742add0d968b55e24f7fd87af1b55dd02a
Kevin Grassi <kmgrassi@gmail.com>
2023-06-09 12:53:29 -0400

    first commit

--- index..rs
+++ index..rs
@@ -1,33 +1,20 @@
-```rust
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use std::io;
-use std::sync::mpsc;
-use std::thread;
+extern crate iron;
+extern crate router;
 
-async fn hello_world() -> impl Responder {
-    HttpResponse::Ok().body("Hello, World!")
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+
+fn main() {
+    let mut router = Router::new();
+    router.get("/", hello_world, "hello_world");
+
+    let server_address = "localhost:3000";
+    println!("Server is running on http://{}", server_address);
+
+    Iron::new(router).http(server_address).unwrap();
 }
 
-#[actix_web::main]
-async fn main() -> io::Result<()> {
-    let (tx, rx) = mpsc::channel();
-    let server = HttpServer::new(|| App::new().route("/", web::get().to(hello_world)))
-        .bind("127.0.0.1:3000")?
-        .shutdown_timeout(60)
-        .run();
-
-    let _ = thread::spawn(move || {
-        let _ = rx.recv();
-        server.stop(true);
-    });
-
-    let _ = tx.clone();
-    ctrlc::set_handler(move || {
-        let _ = tx.send(());
-    })
-    .expect("Error setting Ctrl-C handler");
-
-    println!("Server is running on http://localhost:3000");
-    server.await
+fn hello_world(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "Hello, World!")))
 }
-```
